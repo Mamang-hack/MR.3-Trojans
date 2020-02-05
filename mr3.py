19799fa2a9e9c8df9fa941555be9b8606dbaa92b
MR.3 <59309485+Mamang-hack@users.noreply.github.com>
2020-02-05 13:27:20 +0700

    mr3.py

---
+++ mr3.py
@@ -0,0 +1,52 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f slant " GIESSEN HAWK ATTACK " |lolcat
+echo
+echo
+echo $blue"---------------------------------------"
+echo $blue"AUTOUR  :Mr.3"
+echo $blue"VERSION :1.0"
+echo $blue"JUDUL     :GIESSEN HAWK ATTACK "
+echo $red"---------------------------------------"
+echo $green"?Facebook :Wong Kang Edzan            "
+echo $green"?WhatsApp :+6285781291915              "
+echo $green"?Message :Gunakan dengan bijak     "
+echo $red"---------------------------------------"
+echo $yellow"        GIESSEN HAWK ATTACK"
+echo $red"---------------------------------------"
+echo $white"[1].GIESSEN ATTACK TROJAN"
+echo $white"[2].GIESSEN DROID"
+echo $white"[3].GIESSEN DDOS"
+echo $white"[4].GIESSEN HAMMER"
+echo $white"[5].GIESSEN VIRUS"
+echo $white"[0].EXIT"
+echo $red"---------------------------------------"
+echo $red"MASUKAN PILIHAN ANDA"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant " GIESSEN ATTACK TROJAN " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " GIESSEN DROID " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
